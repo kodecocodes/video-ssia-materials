52ea3e5c9e7d3d9774ffd72bba9ad6fbe627b8fd
Tim <0xtimc@gmail.com>
2022-08-02 17:40:29 +0100

    Add episode 4 projects

---
+++ 04-connect-ios-app-to-vapor-app/final/TILApp/Sources/App/Controllers/CategoriesController.swift
@@ -0,0 +1,63 @@
+/// Copyright (c) 2022 Razeware LLC
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import Vapor
+
+struct CategoriesController: RouteCollection {
+  func boot(routes: RoutesBuilder) throws {
+    let categoriesRoutes = routes.grouped("api", "categories")
+    categoriesRoutes.post(use: createHandler)
+    categoriesRoutes.get(use: getAllHandler)
+    categoriesRoutes.get(":categoryID", use: getHandler)
+    categoriesRoutes.get(":categoryID", "acronyms", use: getAcronymsHandler)
+  }
+  
+  func createHandler(_ req: Request) async throws -> Category {
+    let category = try req.content.decode(Category.self)
+    try await category.save(on: req.db)
+    return category
+  }
+  
+  func getAllHandler(_ req: Request) async throws -> [Category] {
+    try await Category.query(on: req.db).all()
+  }
+  
+  func getHandler(_ req: Request) async throws -> Category {
+    guard let category = try await Category.find(req.parameters.get("categoryID"), on: req.db) else {
+      throw Abort(.notFound)
+    }
+    return category
+  }
+  
+  func getAcronymsHandler(_ req: Request) async throws -> [Acronym] {
+    guard let category = try await Category.find(req.parameters.get("categoryID"), on: req.db) else {
+      throw Abort(.notFound)
+    }
+    return try await category.$acronyms.get(on: req.db)
+  }
+}
